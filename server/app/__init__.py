ba81a747ea604eae2ba7fc07d531e3722ad9ca33
Andey Dogadkin <jvgger@gmail.com>
2024-02-10 06:08:10 +0300

    Fix init flask app.

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,8 +3,8 @@ from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from app.core import AppConfig
 from app.models.base import BaseModel
-from core.config import AppConfig
 
 db = SQLAlchemy(model_class=BaseModel)
 migrate = Migrate()
@@ -16,4 +16,5 @@ db.init_app(app=app)
 migrate.init_app(app=app, db=db)
 marshmallow.init_app(app=app)
 
-from . import api_views  # noqa
+from .views import api_views
+from .models.text_and_count import TextAndCount  # noqa
