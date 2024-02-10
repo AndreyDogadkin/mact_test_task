9c471abf90cf01d542d2da89a3b47b088f62debb
Andey Dogadkin <jvgger@gmail.com>
2024-02-10 22:01:25 +0300

    Fix db methods.

--- server/app/core/services/db_methods.py
+++ server/app/core/services/db_methods.py
@@ -1,29 +1,34 @@
 from sqlalchemy import select
 
 from app import db
-from app.models.text_and_count import TextAndCount
 
 
-def get_last_text_and_count():
-    """Получить последний добавленный объект."""
-    last_post = db.session.execute(
-        select(TextAndCount).order_by(-TextAndCount.id)
-    ).first()
-    return last_post
+class DBMethods:
+    """Класс взаимодействия с базой данных."""
 
+    session = db.session
 
-def add_text_and_count(text, counter):
-    """Добавить объект."""
-    text_and_count = TextAndCount(text=text, counter=counter)
-    with db.session() as s:
-        s.add(text_and_count)
-        s.commit()
-    return get_last_text_and_count()
+    @classmethod
+    def get_last_obj(cls, model):
+        """Получить последний добавленный объект."""
+        query = select(model).order_by(-model.id)
+        with cls.session() as s:
+            last_post = s.execute(query).first()
+        return last_post
 
+    @classmethod
+    def add_obj(cls, model, **kwargs):
+        """Добавить объект."""
+        text_and_count = model(**kwargs)
+        with cls.session() as s:
+            s.add(text_and_count)
+            s.commit()
+        return cls.get_last_obj(model=model)
 
-def get_text_and_count():
-    """Получить все объекты."""
-    query = select(TextAndCount)
-    with db.session() as s:
-        texts_and_counts = s.execute(query)
-        return texts_and_counts.scalars().all()
+    @classmethod
+    def get_list_objs(cls, model):
+        """Получить все объекты."""
+        query = select(model)
+        with cls.session() as s:
+            texts_and_counts = s.execute(query)
+            return texts_and_counts.scalars().all()
--- server/app/views/api_views.py
+++ server/app/views/api_views.py
@@ -4,11 +4,14 @@ import marshmallow
 from flask import request, jsonify
 
 from app import app
-from app.core.services.db_methods import get_text_and_count, add_text_and_count
+from app.core.services.db_methods import DBMethods
+from app.models.text_and_count import TextAndCount
 from app.schemas.text_and_count import TextAndCountSchema
 
 texts_and_counts_schema = TextAndCountSchema(many=True)
 text_and_count_schema = TextAndCountSchema()
+
+database = DBMethods()
 
 
 @app.route("/api/counters/", methods=["GET", "POST"])
@@ -20,14 +23,15 @@ def counters():
         - POST -- data: {"text": <str>, "counter": <int>}
     """
     if request.method == "GET":
-        posts = get_text_and_count()
+        posts = database.get_list_objs(model=TextAndCount)
         posts = texts_and_counts_schema.jsonify(posts)
         return posts, HTTPStatus.OK
     try:
         res = text_and_count_schema.load(request.get_json())
     except marshmallow.ValidationError as e:
         return jsonify({"error": e.messages}), HTTPStatus.BAD_REQUEST
-    text_and_count = add_text_and_count(
+    text_and_count = database.add_obj(
+        model=TextAndCount,
         text=res["text"],
         counter=res["counter"],
     )
