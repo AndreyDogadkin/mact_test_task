885a2fcb6bb943639d6b5790a6a073d0aecf9bd9
Andey Dogadkin <jvgger@gmail.com>
2024-02-10 04:10:24 +0300

    Fix config class doc.

--- server/core/config.py
+++ server/core/config.py
@@ -4,5 +4,7 @@ APP_DIR = Path(__name__).resolve().parent
 
 
 class AppConfig(object):
+    """Конфиг для Flask приложения."""
+
     SQLALCHEMY_DATABASE_URI: str = f"sqlite:///{APP_DIR / 'app.db'}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
